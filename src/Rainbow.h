ae909c598db292fc83c3cd566e2054760824c3a7
Michael.Imelfort <mike@mikeimelfort.com>
2011-09-09 23:55:11 +1000

    Added this guy to help colour graphviz plots

---
+++ src/Rainbow.h
@@ -0,0 +1,59 @@
+// File: Rainbow.h
+// Original Author: Michael Imelfort 2011
+// --------------------------------------------------------------------
+//
+// OVERVIEW:
+//
+// Simple heat map generator
+// 
+// --------------------------------------------------------------------
+//  Copyright  2011 Michael Imelfort and Connor Skennerton
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// --------------------------------------------------------------------
+//
+//                        A
+//                       A B
+//                      A B R
+//                     A B R A
+//                    A B R A C
+//                   A B R A C A
+//                  A B R A C A D
+//                 A B R A C A D A
+//                A B R A C A D A B 
+//               A B R A C A D A B R  
+//              A B R A C A D A B R A 
+//
+
+#ifndef Rainbow_h
+#define Rainbow_h
+
+// system includes
+#include <iostream>
+
+// local includes
+
+// typedefs
+
+class Rainbow 
+{
+    public:
+        Rainbow() {}  
+        ~Rainbow() {}  
+        
+    private:
+        // members
+};
+
+#endif //Rainbow_h
+
