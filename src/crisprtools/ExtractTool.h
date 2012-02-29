d0ac96679ddf32e259aa1aaff94bc08b0aa56ed9
ctSkennerton <c.skennerton@gmail.com>
2012-02-29 12:34:26 +1000

    Extract tool now uses a bit set rather than individual bool members

--- src/crisprtools/ExtractTool.h
+++ src/crisprtools/ExtractTool.h
@@ -1,11 +1,11 @@
 /*
  * ExtractTool.h
  *
- * Copyright (C) 2011 - Connor Skennerton
+ * Copyright (C) 2011 2012 - Connor Skennerton
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -23,6 +23,7 @@
 #include <set>
 #include <iostream>
 #include <fstream>
+#include <bitset>
 #include "Xml.h"
 
 class ExtractTool 
@@ -30,29 +31,10 @@ class ExtractTool
 	public:
     enum ELEMENT_TYPE{REPEAT,SPACER,CONSENSUS,FLANKER};
 		// constructor
-		ExtractTool();
+    ExtractTool();
 
 		// destructor
 		~ExtractTool();
-
-		
-		// get
-		inline bool getDirectRepeat(void) { return ET_DirectRepeat;};
-		inline bool getSpacer(void) { return ET_Spacer;};
-		inline bool getFlanker(void) { return ET_Flanker;};
-		inline std::set<std::string> getGroup(void) { return ET_Group;};
-		inline bool getSplitGroup(void) { return ET_SplitGroup; };
-		inline bool getSplitType(void) { return ET_SplitType;};
-		inline bool getSubset(void) { return ET_Subset; };
-
-		// set
-
-		inline void setDirectRepeat(bool d) { ET_DirectRepeat = d;};
-		inline void setSpacer(bool s) { ET_Spacer = s;};
-		inline void setFlanker(bool f) { ET_Flanker = f;};
-		inline void setGroup(std::set<std::string> g) {ET_Group = g;};
-		inline void setSplitGroup(bool x) { ET_SplitGroup = x;};
-		inline void setSplitType(bool y) {ET_SplitType = y;};
 
 		// option processing
 		void generateGroupsFromString( std::string groupString);
@@ -64,19 +46,28 @@ class ExtractTool
         void extractDataFromGroup(crispr::XML& xmlDoc, xercesc::DOMElement * currentGroup);
     void processData(crispr::XML& xmlDoc, xercesc::DOMElement * currentType, ELEMENT_TYPE wantedType, std::string gid, std::ostream& outStream);
 	private:
-		bool ET_DirectRepeat;						// extract the direct repeat sequences
-		bool ET_Spacer;								// extract the spacer sequences
-		bool ET_Flanker;							// extract the flanking sequences
-		std::set<std::string> ET_Group;             // holds a comma separated list of groups that need to be extracted
-		bool ET_SplitGroup; 						// print results for each group to a separate file
-		bool ET_SplitType;							// print different types of results into different files
-		bool ET_Subset;								// are we doing all groups
-        std::ofstream ET_SpacerStream;              // the current output stream for spacers
+        std::set<std::string> ET_Group;             // holds a comma separated list of groups that need to be extracted
         std::ofstream ET_RepeatStream;
         std::ofstream ET_FlankerStream;
+        std::ofstream ET_SpacerStream;
         std::ofstream ET_OneStream;
         std::ofstream ET_GroupStream;
-    std::string ET_OutputPrefix;
+        std::string ET_OutputPrefix;
+        std::string ET_OutputNamePrefix;
+        std::bitset<7> ET_BitMask;
+/*
+        each bit is for a different option:
+bit pos:  6543210
+          0000000
+          |||||||
+          ||||||------- a subset of the groups is wanted
+          |||||-------- print different types of data (spacers, dr, flanker) into separate files
+          ||||--------- print results from a different group into separate files
+          |||---------- extract flanking sequences
+          ||----------- extract spacer sequences
+          |------------ extract direct repeat sequences
+          ------------- print coverage information if extracting spacers
+ */
 };
 
 
