6948c5dc83784cc6fa7b65010b0db504b5692511
ctSkennerton <c.skennerton@gmail.com>
2013-11-04 14:45:14 +1000

    Basics of new read container written

---
+++ src/crass/Sequence.cpp
@@ -0,0 +1,174 @@
+//
+//  Sequence.cpp
+//  crass
+//
+//  Created by Connor Skennerton on 1/11/13.
+//  Copyright (c) 2013 Australian Centre for Ecogenomics. All rights reserved.
+//
+
+#include "Sequence.h"
+#include <algorithm>
+
+using namespace crass;
+
+
+char comp_tab[] = {
+    0,   1,	2,	 3,	  4,   5,	6,	 7,	  8,   9,  10,	11,	 12,  13,  14,	15,
+    16,  17,  18,	19,	 20,  21,  22,	23,	 24,  25,  26,	27,	 28,  29,  30,	31,
+    32,  33,  34,	35,	 36,  37,  38,	39,	 40,  41,  42,	43,	 44,  45,  46,	47,
+    48,  49,  50,	51,	 52,  53,  54,	55,	 56,  57,  58,	59,	 60,  61,  62,	63,
+    64, 'T', 'V', 'G', 'H', 'E', 'F', 'C', 'D', 'I', 'J', 'M', 'L', 'K', 'N', 'O',
+	'P', 'Q', 'Y', 'S', 'A', 'A', 'B', 'W', 'X', 'R', 'Z',	91,	 92,  93,  94,	95,
+    64, 't', 'v', 'g', 'h', 'e', 'f', 'c', 'd', 'i', 'j', 'm', 'l', 'k', 'n', 'o',
+	'p', 'q', 'y', 's', 'a', 'a', 'b', 'w', 'x', 'r', 'z', 123, 124, 125, 126, 127
+};
+
+std::string crass::reverseComplement(std::string str)
+{
+    
+	int l = static_cast<int>(str.length());
+    char * revcomp_str = new char[l+1];
+    for (int i = 0; i <=l; i++) {
+		revcomp_str[i]='\0';
+    }
+    int i, c0, c1;
+    for (i = 0; i < l>>1; ++i)
+    {
+        c0 = comp_tab[(int)str[i]];
+        c1 = comp_tab[(int)str[l - 1 - i]];
+        revcomp_str[i] = c1;
+        revcomp_str[l - 1 - i] = c0;
+    }
+    if (l&1)
+    {
+        revcomp_str[l>>1] = comp_tab[(int)str[l>>1]];
+    }
+    
+    std::string ret = revcomp_str;
+    delete [] revcomp_str;
+    
+    return ret;
+}
+
+std::string crass::laurenize (std::string seq1)
+{
+    std::string seq2 = reverseComplement(seq1);
+    if (seq1 < seq2)
+    {
+        return seq1;
+    }
+    return seq2;
+}
+
+void RawRead::revComp() {
+    mSeq = reverseComplement(mSeq);
+    mRepeatPositions.reverseRepeatPositions(mSeq.size() - 1);
+    if(!mQual.empty()) {
+        std::reverse(mQual.begin(), mQual.end());
+    }
+    mRepeatLowLexi = !mRepeatLowLexi;
+}
+    
+std::string RawRead::orientateRepeatLowLexi(void)
+{
+    //-----
+    // Orientate a READ based on low lexi of the interalised DR
+    //
+    
+    std::string tmp_dr;
+    std::string rev_comp;
+    
+    int num_repeats = mRepeatPositions.numberOfRepeats();
+    if (num_repeats == 1)
+    {
+        
+        tmp_dr = *(repeatStringBegin());
+        rev_comp = reverseComplement(tmp_dr);
+    }
+    else if (2 == num_repeats)
+    {
+        // choose the dr that is not a partial ( no start at 0 or end at length)
+        
+        // take the second
+        RepeatArray::RepeatIterator it = mRepeatPositions.repeatAt(0);
+        RepeatArray::RepeatIterator it1 = mRepeatPositions.repeatAt(1);
+
+        if ((*it).first == 0)
+        {
+            tmp_dr = *repeatStringAt(1);
+            rev_comp = reverseComplement(tmp_dr);
+        }
+        else if ((*it2).second == static_cast<int>(mSeq.length()))
+        {
+            tmp_dr = repeatStringAt(0);
+            rev_comp = reverseComplement(tmp_dr);
+
+        }
+        else
+        {
+            // if they both are then just take whichever is longer
+            int lenA = (*it).second - (*it).first;
+            int lenB = (*it1).second - (*it1).first;
+            
+            if (lenA > lenB)
+            {
+                tmp_dr = *repeatStringAt(0);
+                rev_comp = reverseComplement(tmp_dr);
+            }
+            else
+            {
+                tmp_dr = *repeatStringAt(1);
+                rev_comp = reverseComplement(tmp_dr);
+            }
+        }
+    }
+    // long read more than two repeats
+    else
+    {
+        // take the second
+        tmp_dr = *repeatStringAt(1);
+        rev_comp = reverseComplement(tmp_dr);
+        
+    }
+    
+    if (tmp_dr < rev_comp)
+    {
+        // the direct repeat is in it lowest lexicographical form
+        mRepeatLowLexi = true;
+#ifdef DEBUG
+        logInfo("DR in low lexi"<<endl<<RH_Seq, 9);
+#endif
+        return tmp_dr;
+    }
+    else
+    {
+        revComp();
+        mRepeatLowLexi = false;
+#ifdef DEBUG
+        logInfo("DR not in low lexi"<<endl<<RH_Seq, 9);
+#endif
+        return rev_comp;
+    }
+}
+
+#ifdef crass_RawRead_main
+int main() {
+    
+    RepeatArray r = RepeatArray();
+    RawRead seq1 = RawRead("1", "my seq", "AAAAAAAGGGGGGG", "@@#$%3", r);
+    seq1.push_back(0, 3);
+    seq1.push_back(7, 12);
+    seq1.inspect();
+    std::cout<<"Testing reverse complement:"<<std::endl;
+    seq1.revComp();
+    seq1.inspect();
+    std::cout<<"Testing repeat string iteration:"<<std::endl;
+    for(auto it = seq1.repeatStringBegin(); it != seq1.repeatStringEnd(); ++it) {
+        std::cout<<*it<<std::endl;
+    }
+    std::cout<<"Testing spacer string iteration:"<<std::endl;
+    for(auto it = seq1.spacerStringBegin(); it != seq1.spacerStringEnd(); ++it) {
+        std::cout<<*it<<std::endl;
+    }
+}
+#endif
---
+++ src/crass/Sequence.h
@@ -0,0 +1,213 @@
+//
+//  Sequence.h
+//  crass
+//
+//  Created by Connor Skennerton on 1/11/13.
+//  Copyright (c) 2013 Australian Centre for Ecogenomics. All rights reserved.
+//
+
+#ifndef __crass__Sequence__
+#define __crass__Sequence__
+
+#include <iostream>
+#include <string>
+
+#include "RepeatArray.h"
+namespace crass {
+
+// global functions
+std::string reverseComplement(std::string str);
+
+std::string laurenize(std::string seq);
+    
+    class RawRead {
+
+    public:
+        
+        class SpacerStringIterator {
+        public:
+            typedef SpacerStringIterator self_t;
+            typedef std::string value_t;
+            typedef value_t& reference_t;
+            typedef value_t* pointer_t;
+            
+            SpacerStringIterator(std::string& input, RepeatArray::SpacerIterator si) : data(input), current_pos(si){}
+            
+            value_t operator *() {
+                return data.substr((*current_pos).first, (*current_pos).second - (*current_pos).first);
+            }
+            
+            self_t operator++() { self_t i = *this; ++current_pos; return i; }
+            self_t operator++(int junk) { ++current_pos; return *this; }
+
+            
+            bool operator==(const self_t& rhs) { return current_pos == rhs.current_pos; }
+            bool operator!=(const self_t& rhs) { return current_pos != rhs.current_pos; }
+            bool operator<(const self_t& rhs) { return current_pos < rhs.current_pos; }
+            bool operator<=(const self_t& rhs) { return current_pos <= rhs.current_pos; }
+            bool operator>(const self_t& rhs) { return current_pos > rhs.current_pos; }
+            bool operator>=(const self_t& rhs) { return current_pos >= rhs.current_pos; }
+            
+            
+        private:
+            std::string& data;
+            RepeatArray::SpacerIterator current_pos;
+        };
+            
+        class RepeatStringIterator {
+        public:
+            typedef RepeatStringIterator self_t;
+            typedef std::string value_t;
+            typedef value_t& reference_t;
+            typedef value_t* pointer_t;
+            
+            RepeatStringIterator(std::string& input, RepeatArray::RepeatIterator si) : data(input), current_pos(si){}
+            
+            value_t operator *() {
+                return data.substr((*current_pos).first, (*current_pos).second - (*current_pos).first);
+            }
+            
+            self_t operator++() { self_t i = *this; ++current_pos; return i; }
+            self_t operator++(int junk) { ++current_pos; return *this; }
+            
+            
+            bool operator==(const self_t& rhs) { return current_pos == rhs.current_pos; }
+            bool operator!=(const self_t& rhs) { return current_pos != rhs.current_pos; }
+            bool operator<(const self_t& rhs) { return current_pos < rhs.current_pos; }
+            bool operator<=(const self_t& rhs) { return current_pos <= rhs.current_pos; }
+            bool operator>(const self_t& rhs) { return current_pos > rhs.current_pos; }
+            bool operator>=(const self_t& rhs) { return current_pos >= rhs.current_pos; }
+                
+                
+            private:
+                std::string& data;
+                RepeatArray::RepeatIterator current_pos;
+            };
+        
+        RawRead()
+        {}
+        
+        RawRead(std::string n, std::string c, std::string s, std::string q, RepeatArray r) :
+                mName(n),
+                mComment(c),
+                mSeq(s),
+                mQual(q),
+                mRepeatPositions(r),
+                mRepeatLowLexi(false),
+                mPrevRead(nullptr),
+                mNextRead(nullptr)
+        {}
+        
+        ~RawRead()
+        {}
+        
+        // get/set
+        void identifier(std::string i) {
+            mName = i;
+        }
+        std::string identifier() {
+            return mName;
+        }
+        
+        void comment(std::string i) {
+            mComment = i;
+        }
+        std::string comment() {
+            return mComment;
+        }
+        
+        void seq(std::string i) {
+            mSeq = i;
+        }
+        std::string seq() {
+            return mSeq;
+        }
+        
+        void quality(std::string i) {
+            mQual = i;
+        }
+        std::string quality() {
+            return mQual;
+        }
+        
+        void repeatLowestLexicographicalForm(bool b) {
+            mRepeatLowLexi = b;
+        }
+        bool repeatLowestLexicographicalForm() {
+            return mRepeatLowLexi;
+        }
+            
+        void nextRead(RawRead *n) {
+            mNextRead = n;
+        }
+        RawRead * nextRead() {
+            return n;
+        }
+        
+        void previousRead(RawRead *p) {
+            mPrevRead = p;
+        }
+        RawRead * previousRead() {
+            return mPrevRead;
+        }
+        
+        void push_back(int s, int e) {
+            mRepeatPositions.add(s, e);
+        }
+            
+        bool firstReadInFragment() {
+            return (mNextRead != nullptr) && (mPrevRead == nullptr);
+        }
+        
+        bool lastReadInFragment() {
+            return (mPrevRead != nullptr) && (mNextRead == nullptr);
+        }
+        
+        void inspect() {
+            std::cout<<"mName="<<mName<<"\n";
+            std::cout<<"mComment="<<mComment<<"\n";
+            std::cout<<"mSeq="<<mSeq<<"\n";
+            std::cout<<"mQual="<<mQual<<"\n";
+            std::cout<<"mRepeatLowLexi="<<mRepeatLowLexi<<"\n";
+            std::cout<<mRepeatPositions<<std::endl;
+            
+        }
+            RepeatArray::RepeatIterator repeatBegin(){return mRepeatPositions.repeatBegin();}
+            RepeatArray::RepeatIterator repeatEnd(){return mRepeatPositions.repeatEnd();}
+            
+            RepeatArray::SpacerIterator spacerBegin(){return mRepeatPositions.spacerBegin();}
+            RepeatArray::SpacerIterator spacerEnd(){return mRepeatPositions.spacerEnd();}
+            
+            RawRead::SpacerStringIterator spacerStringBegin(){return SpacerStringIterator(mSeq, mRepeatPositions.spacerBegin());}
+            RawRead::SpacerStringIterator spacerStringEnd(){return SpacerStringIterator(mSeq, mRepeatPositions.spacerEnd());}
+            
+            RawRead::RepeatStringIterator repeatStringBegin(){return RepeatStringIterator(mSeq, mRepeatPositions.repeatBegin());}
+            RawRead::RepeatStringIterator repeatStringEnd(){return RepeatStringIterator(mSeq, mRepeatPositions.repeatEnd());}
+            
+            RawRead::RepeatStringIterator repeatStringAt(int i){return RepeatStringIterator(mSeq, mRepeatPositions.repeatAt(i));}
+            RawRead::SpacerStringIterator spacerStringAt(int i){return SpacerStringIterator(mSeq, mRepeatPositions.spacerAt(i));}
+            
+        
+        void revComp();
+            
+        std::string orientateRepeatLowLexi();
+        
+
+
+
+        
+    private:
+        
+        //members
+        std::string mName;
+        std::string mComment;
+        std::string mSeq;
+        std::string mQual;
+        RepeatArray mRepeatPositions;
+        bool mRepeatLowLexi;
+        RawRead *mPrevRead;
+        RawRead *mNextRead;
+    };
+
+}
+#endif /* defined(__crass__RawRead__) */
