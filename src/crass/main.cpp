2cd45b4b389e20039dda18c5dd55850a3df55253
ctSkennerton <c.skennerton@gmail.com>
2014-01-17 14:35:21 +1000

    changing over iostreams to cstdio and output the graphs to file

--- src/crass/main.cpp
+++ src/crass/main.cpp
@@ -39,6 +39,7 @@
 #include "Graph.h"
 
 #include <iostream>
+#include <cstdio>
 #include <fstream>
 #include <sys/stat.h>
 #include <getopt.h>
@@ -58,39 +59,35 @@ void RecursiveMkdir(std::string dir)
 
 void versionInfo(void)
 {
-    std::cout<<PACKAGE_NAME<<" version "<<PACKAGE_VERSION<<std::endl<<std::endl;
-    std::cout<<"---------------------------------------------------------------"<<std::endl;
-    std::cout<<"Copyright (C) 2011-2013 Connor Skennerton & Michael Imelfort"<<std::endl;
-    std::cout<<"This program comes with ABSOLUTELY NO WARRANTY"<<std::endl;
-    std::cout<<"This is free software, and you are welcome to redistribute it"<<std::endl;
-    std::cout<<"under certain conditions: See the source for more details"<<std::endl;
-    std::cout<<"---------------------------------------------------------------"<<std::endl;
+    printf("%s version %s\n\n",PACKAGE_NAME,PACKAGE_VERSION);
+    puts("---------------------------------------------------------------");
+    puts("Copyright (C) 2011-2013 Connor Skennerton & Michael Imelfort");
+    puts("This program comes with ABSOLUTELY NO WARRANTY");
+    puts("This is free software, and you are welcome to redistribute it");
+    puts("under certain conditions: See the source for more details");
+    puts("---------------------------------------------------------------");
 }
 
 void usage( ) {
-    std::cout<<std::endl;
-    std::cout<<"Usage:  "<<std::endl;
-    std::cout<<PACKAGE_NAME<<"  [options] <inputFile>..."<<std::endl;
+    puts("\nUsage:");
+    puts(PACKAGE_NAME"  [options] <inputFile>...\n");
     //std::cout<<PACKAGE_NAME<<"  [options] -ps (<readfile1> <readfile2>)..."<<std::endl;
     //std::cout<<PACKAGE_NAME<<"  [options] -pi <readfile12>..."<<std::endl;
 
-    std::cout<<"Options:"<<std::endl;
-    std::cout<< "-h --help                    This help message"<<std::endl;
-    std::cout<< "-l --logLevel        <INT>   Output a log file and set a log level [1 - "<<CRASS_DEF_MAX_LOGGING<<"]"<<std::endl;
-    std::cout<< "-L --logFile         <FILE>  Name of the file for log in the output directory."<<std::endl;
-    std::cout<< "                             use 'stdout' or 'stderr' to print logging info to that stream"<<std::endl;
-    std::cout<< "                             [Default: log.txt]"<<std::endl;
-    std::cout<< "-o --outDir          <DIR>   Output directory; will be created if it doesn't exist [default: .]"<<std::endl;
-    std::cout<< "-V --version                 Program and version information"<<std::endl;
-    std::cout<< "-d --minDR           <INT>   Minimim length of the direct repeat"<<std::endl;
-    std::cout<< "                             to search for [Default: "<<CRASS_DEF_MIN_DR_SIZE<<"]"<<std::endl;
-    std::cout<< "-D --maxDR           <INT>   Maximim length of the direct repeat"<<std::endl;
-    std::cout<< "                             to search for [Default: "<<CRASS_DEF_MAX_DR_SIZE<<"]"<<std::endl;
-    std::cout<< "-s --minSpacer       <INT>   Minimim length of the spacer to search for [Default: "<<CRASS_DEF_MIN_SPACER_SIZE<<"]"<<std::endl;
-    std::cout<< "-S --maxSpacer       <INT>   Maximim length of the spacer to search for [Default: "<<CRASS_DEF_MAX_SPACER_SIZE<<"]"<<std::endl;
-    std::cout<< "-f --covCutoff       <INT>   Remove groups with less than x spacers [Default: "<<CRASS_DEF_COVCUTOFF<<"]"<<std::endl;
-    std::cout<< "-k --clusterCutoff   <REAL>  The sequence identity between two repeats for clustering [Default: "<<0.9<<"]"<<std::endl;
-    std::cout<<std::endl;
+    puts("Options:");
+    printf("%-20s %-10s%s\n", "-h --help","","This help message");
+    printf("%-20s %-10s%s%d%s\n", "-l --logLevel","<INT>", "Output a log file and set a log level [1 - ",CRASS_DEF_MAX_LOGGING,"]");
+    printf("%-20s %-10s%s\n", "-L --logFile", "<FILE>", "Name of the file for log in the output directory.");
+    printf("%-20s %-10s%s\n", "", "", "use 'stdout' or 'stderr' to print logging info to that stream [Default: log.txt]");
+    printf("%-20s %-10s%s\n", "-o --outDir", "<DIR>","Output directory; will be created if it doesn't exist [default: .]");
+    printf("%-20s %-10s%s\n", "-V --version","","Program and version information");
+    printf("%-20s %-10s%s%d%s\n","-d --minDR","<INT>","Minimim length of the direct repeat to search for [Default: ",CRASS_DEF_MIN_DR_SIZE,"]");
+    printf("%-20s %-10s%s%d%s\n","-D --maxDR","<INT>","Maximim length of the direct repeat to search for [Default: ",CRASS_DEF_MAX_DR_SIZE,"]");
+    printf("%-20s %-10s%s%d%s\n","-s --minSpacer","<INT>","Minimim length of the spacer to search for [Default: ", CRASS_DEF_MIN_SPACER_SIZE, "]");
+    printf("%-20s %-10s%s%d%s\n","-S --maxSpacer","<INT>","Maximim length of the spacer to search for [Default: ",CRASS_DEF_MAX_SPACER_SIZE,"]");
+    printf("%-20s %-10s%s%d%s\n","-f --covCutoff","<INT>","Remove groups with less than x spacers [Default: ",CRASS_DEF_COVCUTOFF,"]");
+    printf("%-20s %-10s%s%.1f%s\n", "-k --clusterCutoff","<REAL>","The sequence identity between two repeats for clustering [Default: ",0.9,"]");
+    puts("");
 
 
 }
@@ -251,22 +248,47 @@ int main(int argc, char * argv[]) {
             searcher.searchFileForPatterns(argv[file_begin]);
         }
         
-        read_store.inspect(std::cout);
-        std::ofstream reads_dump(opts.outdir + "/reads");
-        if (reads_dump.good()) {
-            read_store.dumpReads(reads_dump);
-        }
-        reads_dump.close();
+        //read_store.inspect(std::cout);
+        //std::ofstream reads_dump(opts.outdir + "/reads");
+        //if (reads_dump.good()) {
+        //    read_store.dumpReads(reads_dump);
+        //}
+        //reads_dump.close();
+        
         // repeat cluster is map of clust_id (int) to list of repeats (StringToken)
         for(auto repeat_clust_it = read_store.repeatClusterBegin(); repeat_clust_it != read_store.repeatClusterEnd(); ++repeat_clust_it) {
             crass::Graph crispr_grapher = crass::Graph(21);
+
             // dr_list_it is std::list<StringToken>::iterator for repeats
             for(auto dr_list_it = repeat_clust_it->second.begin(); dr_list_it != repeat_clust_it->second.end(); ++dr_list_it) {
                 for(auto read_it = read_store.readFromRepeatBegin(*dr_list_it); read_it != read_store.readFromRepeatEnd(*dr_list_it); ++read_it) {
                     crispr_grapher.addReadToGraph(*read_it);
                 }
             }
-            crispr_grapher.toGraphviz(std::cout);
+            char graph_name[20];
+            snprintf(graph_name, 20, "cluster_%d", repeat_clust_it->first);
+            std::deque<crass::Node *> rpath;
+            crispr_grapher.identifyRepeatNodes(rpath);
+            printf("%s Length=%ld\n", graph_name, rpath.size()+21);
+
+            
+            auto dr_it = rpath.begin();
+            std::string kmer;
+            kmer = crispr_grapher.kmerStr((*dr_it)->mId);
+            printf("%s", kmer.c_str());
+            for(++dr_it; dr_it != rpath.end(); ++dr_it) {
+                std::string kmer = crispr_grapher.kmerStr((*dr_it)->mId);
+                putc(kmer.back(), stdout);
+            }
+            putc('\n', stdout);
+            char graph_file_name[1024];
+            strcpy(graph_file_name, opts.outdir.c_str());
+            strcat(graph_file_name, "/");
+            strcat(graph_file_name, graph_name);
+            strcat(graph_file_name, ".gv");
+            FILE * graph_file = fopen(graph_file_name, "w");
+            crispr_grapher.toGraphviz(graph_file, graph_name);
+            fclose(graph_file);
         }
 
     } else {
