2d7b11e1ef7e28cf5c9766ee8bdc0587f3468f3b
ctSkennerton <c.skennerton@gmail.com>
2016-06-23 15:15:45 -0700

    add in more unit tests

--- src/test/test_libcrispr.cpp
+++ src/test/test_libcrispr.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 #include "catch.hpp"
 #include "libcrispr.h"
 #include "ReadHolder.h"
@@ -7,26 +9,33 @@
 // 0         1         2         3         4         5         6         7         8         9         0
 // 01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
 // CCCCGCAGGCGCGGGGATGAACCGAGCGAGACATCACCGGCGAGTCGGAGCGCGTTGCGTTCCCCGCAGGCGCGGGGATGAACCGAAGATAAACGCCGGCG
-// RRRRRRRRRRRRRRRRRRRRRRRRRSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSRRRRRRRRRRRRRRRRRRRRRRRRRSSSSSSSSSSSSSSS
-// CCCCGCAGGCGCGGGGATGAACCGA
-// |||||||||||||||||||||||||
-// CCCCGCAGGCGCGGGGATGAACCGA
-
-// read2
-// 0                                                                                                   1                                                 
-// 0         1         2         3         4         5         6         7         8         9         0         1         2         3         4         5         6         7         8         9
-// 01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
-// CTTCCACTAACCATTTCCCCGTAAGGGGACGGAAACATTTTTTATCCAGATTTTTCCCCAAATTTGCAATAATTGCTACTTCCACTAACCATTTCCCCGTAAGGGGACGGAAACTTCTGTAGAGTTATTGTATAAGAACCCCACGTAGAAACGAGCTTCCACTAACCATTTCCCCGTAAGGGGACGGAAAC
-// RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR                                          RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR                                         RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
-/*
-TEST_CASE("check reverse complementing using DR", "[ReadHolder]") {
+// RRRRRRRRRRRRRRRRRRRRRRRRR                                    RRRRRRRRRRRRRRRRRRRRRRRRR               
+TEST_CASE("searching for additional repeated kmers in 100bp read", "[libcrispr]"){
     ReadHolder read("CCCCGCAGGCGCGGGGATGAACCGAGCGAGACATCACCGGCGAGTCGGAGCGCGTTGCGTTCCCCGCAGGCGCGGGGATGAACCGAAGATAAACGCCGGCG",
                     "HWI-EAS165_0052:2:58:8891:11288#CGATGT/1_C233_C23");
-    read.startStopsAdd(0, 24);
-    read.startStopsAdd(61,85);
+    SECTION("where there should be no additional matches with a minimum spacer length of 21"){
+        read.startStopsAdd(0, 8);
+        read.startStopsAdd(61,69);
+        std::string pattern = "CCCCGCAGG";
+        scanRight(read, pattern, 21, 24);
+        StartStopList reppos = read.getStartStopList();
+        REQUIRE(reppos[0] == 0);
+        REQUIRE(reppos[1] == 8);
+        REQUIRE(reppos[2] == 61);
+        REQUIRE(reppos[3] == 69);
+    }
+    SECTION("where there should be no additional matches with a minimum spacer length of 10"){
+        read.startStopsAdd(0, 8);
+        read.startStopsAdd(61,69);
+        std::string pattern = "CCCCGCAGG";
+        scanRight(read, pattern, 10, 24);
+        StartStopList reppos = read.getStartStopList();
+        REQUIRE(reppos[0] == 0);
+        REQUIRE(reppos[1] == 8);
+        REQUIRE(reppos[2] == 61);
+        REQUIRE(reppos[3] == 69);
+    }
 }
-*/
-
 TEST_CASE("check extending repeat with 100bp read", "[libcrispr]") {
     ReadHolder read("CCCCGCAGGCGCGGGGATGAACCGAGCGAGACATCACCGGCGAGTCGGAGCGCGTTGCGTTCCCCGCAGGCGCGGGGATGAACCGAAGATAAACGCCGGCG",
                     "HWI-EAS165_0052:2:58:8891:11288#CGATGT/1_C233_C23");
@@ -34,7 +43,8 @@ TEST_CASE("check extending repeat with 100bp read", "[libcrispr]") {
     SECTION("The search window length is 8 and the min spacer length is 21") {
         read.startStopsAdd(0, 8);
         read.startStopsAdd(61,69);
-        extendPreRepeat(read, 8, 21);
+        int repeat_length = extendPreRepeat(read, 8, 21);
+        REQUIRE(repeat_length == 25);
         StartStopList reppos = read.getStartStopList();
         REQUIRE(reppos[0] == 0);
         REQUIRE(reppos[1] == 24);
@@ -44,7 +54,8 @@ TEST_CASE("check extending repeat with 100bp read", "[libcrispr]") {
     SECTION("The search window length is 6 and the min spacer length is 21") {
         read.startStopsAdd(0, 6);
         read.startStopsAdd(61,67);
-        extendPreRepeat(read, 6, 21);
+        int repeat_length = extendPreRepeat(read, 6, 21);
+        REQUIRE(repeat_length == 25);
         StartStopList reppos = read.getStartStopList();
         REQUIRE(reppos[0] == 0);
         REQUIRE(reppos[1] == 24);
@@ -54,12 +65,49 @@ TEST_CASE("check extending repeat with 100bp read", "[libcrispr]") {
     SECTION("The search window length is 11 and the min spacer length is 21") {
         read.startStopsAdd(0, 11);
         read.startStopsAdd(61,72);
-        extendPreRepeat(read, 11, 21);
+        int repeat_length = extendPreRepeat(read, 11, 21);
+        REQUIRE(repeat_length == 25);
         StartStopList reppos = read.getStartStopList();
         REQUIRE(reppos[0] == 0);
         REQUIRE(reppos[1] == 24);
         REQUIRE(reppos[2] == 61);
         REQUIRE(reppos[3] == 85);
+    }
+}
+// read2
+// 0                                                                                                   1                                                 
+// 0         1         2         3         4         5         6         7         8         9         0         1         2         3         4         5         6         7         8         9
+// 01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+// CTTCCACTAACCATTTCCCCGTAAGGGGACGGAAACATTTTTTATCCAGATTTTTCCCCAAATTTGCAATAATTGCTACTTCCACTAACCATTTCCCCGTAAGGGGACGGAAACTTCTGTAGAGTTATTGTATAAGAACCCCACGTAGAAACGAGCTTCCACTAACCATTTCCCCGTAAGGGGACGGAAAC
+// RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR                                          RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR                                         RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
+TEST_CASE("searching for additional repeated kmers in 190bp read", "[libcrispr]"){
+    ReadHolder read2("CTTCCACTAACCATTTCCCCGTAAGGGGACGGAAACATTTTTTATCCAGATTTTTCCCCAAATTTGCAATAATTGCTACTTCCACTAACCATTTCCCCGTAAGGGGACGGAAACTTCTGTAGAGTTATTGTATAAGAACCCCACGTAGAAACGAGCTTCCACTAACCATTTCCCCGTAAGGGGACGGAAAC",
+            "NC_019753_1");
+    SECTION("where there should be one additional match with a minimum spacer length of 21"){
+        read2.startStopsAdd(0, 8);
+        read2.startStopsAdd(78,86);
+        std::string pattern = "CTTCCACT";
+        scanRight(read2, pattern, 21, 24);
+        StartStopList reppos = read2.getStartStopList();
+        REQUIRE(reppos[0] == 0);
+        REQUIRE(reppos[1] == 8);
+        REQUIRE(reppos[2] == 78);
+        REQUIRE(reppos[3] == 86);
+        REQUIRE(reppos[4] == 155);
+        REQUIRE(reppos[5] == 163);
+    }
+    SECTION("where there should be one additional match with a minimum spacer length of 10"){
+        read2.startStopsAdd(0, 8);
+        read2.startStopsAdd(78,86);
+        std::string pattern = "CTTCCACT";
+        scanRight(read2, pattern, 10, 24);
+        StartStopList reppos = read2.getStartStopList();
+        REQUIRE(reppos[0] == 0);
+        REQUIRE(reppos[1] == 8);
+        REQUIRE(reppos[2] == 78);
+        REQUIRE(reppos[3] == 86);
+        REQUIRE(reppos[4] == 155);
+        REQUIRE(reppos[5] == 163);
     }
 }
 TEST_CASE("check extending repeat with 190bp read", "[libcrispr]") {
@@ -71,7 +119,8 @@ TEST_CASE("check extending repeat with 190bp read", "[libcrispr]") {
         read2.startStopsAdd(0,8);
         read2.startStopsAdd(78,86);
         read2.startStopsAdd(155,163);
-        extendPreRepeat(read2, 8, 21);
+        int repeat_length = extendPreRepeat(read2, 8, 21);
+        REQUIRE(repeat_length == 36);
         StartStopList reppos = read2.getStartStopList();
 
         REQUIRE(reppos[0] == 0);
@@ -85,7 +134,8 @@ TEST_CASE("check extending repeat with 190bp read", "[libcrispr]") {
         read2.startStopsAdd(0,6);
         read2.startStopsAdd(78,84);
         read2.startStopsAdd(155,161);
-        extendPreRepeat(read2, 6, 21);
+        int repeat_length = extendPreRepeat(read2, 6, 21);
+        REQUIRE(repeat_length == 36);
         StartStopList reppos = read2.getStartStopList();
 
         REQUIRE(reppos[0] == 0);
@@ -99,7 +149,8 @@ TEST_CASE("check extending repeat with 190bp read", "[libcrispr]") {
         read2.startStopsAdd(0,11);
         read2.startStopsAdd(78,89);
         read2.startStopsAdd(155,166);
-        extendPreRepeat(read2, 11, 21);
+        int repeat_length = extendPreRepeat(read2, 11, 21);
+        REQUIRE(repeat_length == 36);
         StartStopList reppos = read2.getStartStopList();
 
         REQUIRE(reppos[0] == 0);
@@ -108,5 +159,71 @@ TEST_CASE("check extending repeat with 190bp read", "[libcrispr]") {
         REQUIRE(reppos[3] == 113);
         REQUIRE(reppos[4] == 155);
         REQUIRE(reppos[5] == 190);
+    }
+}
+// read3
+// 0                                                                                                   1                                                                                                   2                                                                                                   3
+// 0         1         2         3         4         5         6         7         8         9         0         1         2         3         4         5         6         7         8         9         0         1         2         3         4         5         6         7         8         9         0     
+// 0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+// CTTCCACTAACCATTTCCCCGTAAGGGGACGGAAACATTTTTTATCCAGATTTTTCCCCAAATTTGCAATAATTGCTACTTCCACTAACCATTTCCCCGTAAGGGGACGGAAACTTCTGTAGAGTTATTGTATAAGAACCCCACGTAGAAACGAGCTTCCACTAACCATTTCCCCGTAAGGGGACGGAAACCAGAAATTACGACAGACGCGCAAGCAGGATCAGCCACTTCCACTAACCATTTCCCCGTAAGGGGACGGAAACAGCCAGATAATGATGATAATGCTAGAGGCTGTCCGTT
+// RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR                                          RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR                                         RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR                                    RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
+TEST_CASE("check extending repeat with 300bp read", "[libcrispr]") {
+
+    ReadHolder read2("CTTCCACTAACCATTTCCCCGTAAGGGGACGGAAACATTTTTTATCCAGATTTTTCCCCAAATTTGCAATAATTGCTACTTCCACTAACCATTTCCCCGTAAGGGGACGGAAACTTCTGTAGAGTTATTGTATAAGAACCCCACGTAGAAACGAGCTTCCACTAACCATTTCCCCGTAAGGGGACGGAAACCAGAAATTACGACAGACGCGCAAGCAGGATCAGCCACTTCCACTAACCATTTCCCCGTAAGGGGACGGAAACAGCCAGATAATGATGATAATGCTAGAGGCTGTCCGTT",
+            "NC_019753_1");
+
+    SECTION("The search window length is 8 and the min spacer length is 21") {
+        read2.startStopsAdd(0,8);
+        read2.startStopsAdd(78,86);
+        read2.startStopsAdd(155,163);
+        read2.startStopsAdd(227,235);
+        int repeat_length = extendPreRepeat(read2, 8, 21);
+        REQUIRE(repeat_length == 36);
+        StartStopList reppos = read2.getStartStopList();
+
+        REQUIRE(reppos[0] == 0);
+        REQUIRE(reppos[1] == 35);
+        REQUIRE(reppos[2] == 78);
+        REQUIRE(reppos[3] == 113);
+        REQUIRE(reppos[4] == 155);
+        REQUIRE(reppos[5] == 190);
+        REQUIRE(reppos[6] == 227);
+        REQUIRE(reppos[7] == 262);
+    }
+    SECTION("The search window length is 6 and the min spacer length is 21") {
+        read2.startStopsAdd(0,6);
+        read2.startStopsAdd(78,84);
+        read2.startStopsAdd(155,161);
+        read2.startStopsAdd(227,233);
+        int repeat_length = extendPreRepeat(read2, 6, 21);
+        REQUIRE(repeat_length == 36);
+        StartStopList reppos = read2.getStartStopList();
+
+        REQUIRE(reppos[0] == 0);
+        REQUIRE(reppos[1] == 35);
+        REQUIRE(reppos[2] == 78);
+        REQUIRE(reppos[3] == 113);
+        REQUIRE(reppos[4] == 155);
+        REQUIRE(reppos[5] == 190);
+        REQUIRE(reppos[6] == 227);
+        REQUIRE(reppos[7] == 262);
+    }
+    SECTION("The search window length is 11 and the min spacer length is 21") {
+        read2.startStopsAdd(0,11);
+        read2.startStopsAdd(78,89);
+        read2.startStopsAdd(155,166);
+        read2.startStopsAdd(227,238);
+        int repeat_length = extendPreRepeat(read2, 11, 21);
+        REQUIRE(repeat_length == 36);
+        StartStopList reppos = read2.getStartStopList();
+
+        REQUIRE(reppos[0] == 0);
+        REQUIRE(reppos[1] == 35);
+        REQUIRE(reppos[2] == 78);
+        REQUIRE(reppos[3] == 113);
+        REQUIRE(reppos[4] == 155);
+        REQUIRE(reppos[5] == 190);
+        REQUIRE(reppos[6] == 227);
+        REQUIRE(reppos[7] == 262);
     }
 }
